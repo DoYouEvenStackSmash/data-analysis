73142e634f89d676317412e5dbcab53bc41e0b59
Jake <manumoomaw@gmail.com>
2023-09-09 10:48:39 -0700

    added comments

--- src/cpp-processing/kmedioids.h
+++ src/cpp-processing/kmedioids.h
@@ -1,47 +1,61 @@
-#include <iostream>
-#include <vector>
-#include <eigen3/Eigen/Dense>
-#include <ctime>
-#include <algorithm>
+#include <iostream>                      // Standard I/O operations
+#include <vector>                        // Dynamic arrays (std::vector)
+#include <eigen3/Eigen/Dense>            // Eigen library for matrix operations
+#include <ctime>                         // Time-related functions
+#include <algorithm>                    // Algorithms (e.g., std::min_element)
 
-// Assigns data points to clusters based on distances to medioids.
-// Modifies the ref_clusters vector to store the assigned clusters.
+// Function to assign data points to clusters based on distances to medioids.
+// - 'distances' is the pairwise distance matrix between data points.
+// - 'medioid_indices' contains the indices of current medioids.
+// - 'ref_clusters' will store the assigned data point indices for each medioid.
 void assign_clusters(
-    Eigen::MatrixXf &distances,
-    std::vector<int> &medioid_indices,
-    std::vector<std::vector<int>> &ref_clusters
+    Eigen::MatrixXf &distances,              // Pairwise distance matrix
+    std::vector<int> &medioid_indices,      // Indices of current medioids
+    std::vector<std::vector<int>> &ref_clusters  // Assigned data point indices for each medioid
 );
 
-// Computes the pairwise distance matrix between data points and stores it in 'distances'.
+// Function to compute the pairwise distance matrix between data points.
+// - 'data' is an array of data points represented as Eigen::MatrixXf.
+// - 'n' is the number of data points.
+// - 'distances' is an output parameter to store the computed pairwise distance matrix.
 void computeDistanceMatrix(
-    Eigen::MatrixXf *data,
-    int n,
-    Eigen::MatrixXf *distances
+    Eigen::MatrixXf *data,                 // Array of data points
+    int n,                                 // Number of data points
+    Eigen::MatrixXf *distances             // Output parameter for pairwise distance matrix
 );
 
-// Preprocesses the data according to the k-medioids algorithm.
-// Returns a list of indices referring to medioids in 'medioidIndices'.
-// Computes the distance matrix and stores it in 'return_distances'.
+// Function to preprocess data for k-medioids clustering.
+// - 'data' is an array of data points represented as Eigen::MatrixXf.
+// - 'n' is the number of data points.
+// - 'k' is the number of medioids to be selected.
+// - 'medioidIndices' will store the indices of selected medioids.
+// - 'return_distances' is an output parameter to store the computed pairwise distance matrix.
 void preprocess(
-    Eigen::MatrixXf *data,
-    int n,
-    int k,
-    std::vector<int> &medioidIndices,
-    Eigen::MatrixXf *return_distances
+    Eigen::MatrixXf *data,                 // Array of data points
+    int n,                                 // Number of data points
+    int k,                                 // Number of medioids
+    std::vector<int> &medioidIndices,      // Output: Indices of selected medioids
+    Eigen::MatrixXf *return_distances     // Output: Pairwise distance matrix
 );
 
-// Updates the medioids based on the assigned clusters and distances.
+// Function to update medioids based on assigned clusters.
+// - 'distances' is the pairwise distance matrix between data points.
+// - 'medioid_indices' contains the indices of current medioids.
+// - 'ref_clusters' stores the assigned data point indices for each medioid.
 void update_medioids(
-    Eigen::MatrixXf &distances,
-    std::vector<int> &medioid_indices,
-    std::vector<std::vector<int>> &ref_clusters
+    Eigen::MatrixXf &distances,              // Pairwise distance matrix
+    std::vector<int> &medioid_indices,      // Indices of current medioids
+    std::vector<std::vector<int>> &ref_clusters  // Assigned data point indices for each medioid
 );
 
-// Calculates the total sum of distances between data points and their respective medioids.
-// The result is stored in 'total_sum'.
+// Function to calculate the total sum of distances between data points and medioids.
+// - 'distances' is the pairwise distance matrix between data points.
+// - 'medioid_indices' contains the indices of current medioids.
+// - 'ref_clusters' stores the assigned data point indices for each medioid.
+// - 'total_sum' is an output parameter to store the calculated total sum.
 void calculate_sum(
-    Eigen::MatrixXf &distances,
-    std::vector<int> &medioid_indices,
-    std::vector<std::vector<int>> &ref_clusters,
-    double *total_sum
+    Eigen::MatrixXf &distances,              // Pairwise distance matrix
+    std::vector<int> &medioid_indices,      // Indices of current medioids
+    std::vector<std::vector<int>> &ref_clusters,  // Assigned data point indices for each medioid
+    double *total_sum                        // Output: Calculated total sum
 );
